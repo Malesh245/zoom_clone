176f775d93c7cc6a196032d1bfbe24d276786736
Malesh Kumar <maleshkumar12345@gmail.com>
2025-02-12 11:13:04 +0530

    some change

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -56,6 +56,8 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
 
         setRecordings(recordings);
       } catch (error) {
+        console.log(error);
+
         toast({ title: "Try again later" });
       }
     };
